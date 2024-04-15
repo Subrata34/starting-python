4d6b2302991fb463ced4ea36e597c027f5c83eb4
Subrata34 <subratamodak3491@gmail.com>
2024-04-15 11:54:29 +0600

    files

--- boolian.py
+++ boolian.py
@@ -21,3 +21,13 @@ print(type(x))
 number_1=20;
 number_2=80;
 print(f"my age is {number_2}and my roll number is {number_1}")
+#Binary type data 
+somenumber=[3,5,7,9,67,79,89,49,49,240] #byte range 256
+b=bytes(somenumber)
+print(type(b))
+#binary type data array 
+somenumber=[3,5,7,9,67,79,89,49,49,240] #byte range 256
+b1=bytearray(somenumber)
+print(type(b1))
+b1[0]=150
+print(b1[0])
