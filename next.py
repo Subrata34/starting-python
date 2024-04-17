4324370ad83de3b7016f42b9429ef55dbdd7ad2a
Subrata34 <subratamodak3491@gmail.com>
2024-04-17 10:54:49 +0600

    files

--- next.py
+++ next.py
@@ -14,3 +14,30 @@ print(list)
 list[2]='facebook'
 print(list)
 
+# append {this function is add data in this list  }
+list.append('google chorme')
+print(list )
+
+#insert {this function is add data by index number }
+
+list.insert(3,'Youtube ')
+print(list )
+
+#remove {this function is remove the data from list }
+newlist =['habib ','mamun','jamil','khairul','kumkum',800,900,500,400]
+print(newlist)
+newlist.remove('jamil')
+print(newlist)
+
+#pop {this function is remove index number }
+newlist.pop(1)
+print(newlist)
+
+#del {this function is remove one index from this list }
+del newlist[1]
+print(newlist)
+
+#clear {this function is remove in the world }
+print(newlist)
+newlist.clear()
+print(newlist)
