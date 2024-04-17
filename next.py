23b88775e6f502196890d361dde4b1d66ba0bb61
Subrata34 <subratamodak3491@gmail.com>
2024-04-17 10:32:41 +0600

    files

--- next.py
+++ next.py
@@ -1,3 +1,16 @@
 #type casting : converting work type casting 
 name=40
 print(int(name))   #this convert iniger from string 
+
+#python list :
+li=[1,90,200]
+print(li)
+li[1]=20
+print(li) 
+
+# Access list item :
+list =['google','gmail','instagram','microsoft']
+print(list)
+list[2]='facebook'
+print(list)
+
