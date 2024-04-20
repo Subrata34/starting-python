d49bb8d3a2ccf1ee847a853955b5e3bbc2e6241e
Subrata34 <subratamodak3491@gmail.com>
2024-04-20 18:24:47 +0600

    files

--- tuple.py
+++ tuple.py
@@ -2,4 +2,11 @@ newtuple=[1,2,3,'vaggo',True,False]
 
 print(newtuple[-2])     #index start from -1 and lifte site
 #Rang of  tuble 
-print(newtuple[-4:-1])
+print(newtuple[1:6])
+
+#update tuple 
+thistuple=['hamdu','habib','khokon','sakib']
+a=list(thistuple) #convert to the list tuple and then append 
+a.append("sakil") #append this tuple 
+thistuple=tuple(a) #store the tuple 
+print(thistuple)
