d756c9a1ad0f21548035ed0ff5c36f7db5fddc0e
Subrata34 <subratamodak3491@gmail.com>
2024-04-22 17:15:16 +0600

    files

---
+++ looptuple.py
@@ -0,0 +1,16 @@
+fruits=( 'chari','mango','bananna ','apple ','watermalon')
+for i in fruits:
+    print(i)
+#call index number 
+for x in range(len(fruits)):
+    print(x)
+
+#we call the name from index number 
+for x in range(len(fruits)):
+   print(fruits[x])
+#when loop apply 
+t=0
+while t<len(fruits):
+   print(fruits[t])
+   t=t+1
+
