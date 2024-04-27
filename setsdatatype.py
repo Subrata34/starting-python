3ca536dc8f7c8b5ebe6211cb0c42b3ed2a2cfd58
Subrata34 <subratamodak3491@gmail.com>
2024-04-27 16:21:02 +0600

    files

--- setsdatatype.py
+++ setsdatatype.py
@@ -5,3 +5,11 @@ print(myset)
 MYset={1,2,'3',4,1,2}
 for i in MYset:
     print(i)
+
+print(4 in MYset)
+
+#Add set Item 
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
