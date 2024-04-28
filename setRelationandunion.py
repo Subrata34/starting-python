68f8ccee31a4361b99f257b1bde14ccafadff35a
Subrata34 <subratamodak3491@gmail.com>
2024-04-28 13:22:26 +0600

    files

---
+++ setRelationandunion.py
@@ -0,0 +1,10 @@
+#joint two variable by union 
+Myset1={1,2,3,4,5,6,7,8}
+Myset2={'a','b','c','d','g','o','p'}
+set=Myset1.union(Myset2)
+print(set)
+
+#Again set joint
+
+Myset1.update(Myset2)
+print("Joint the two variable : ",Myset1)
