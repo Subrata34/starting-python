72e44b6c5a5a0cbc5e9ac3e9ee90e21ecf818bb8
Subrata34 <subratamodak3491@gmail.com>
2024-04-30 18:41:04 +0600

    files

--- python_dictionarys.py
+++ python_dictionarys.py
@@ -1,5 +1,18 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+studentInfo={
+    "Labib" :{
+        "Location" :"Banani",
+        "study"    :"12",
+        "Subject"  :"Science",
+        "Roll"     :12,
+        "Number"   : 17499903987
+    },
+    "Habib" :{
+        "Location" :"Banani",
+        "study"    :"12",
+        "Subject"  :"Science",
+        "Roll"     :20,
+        "Number"   : 17499903987677
+
+    }
 }
+print(studentInfo["Labib"]["Number"])
