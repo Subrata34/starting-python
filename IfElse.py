a3fe8b5a9ca6e897b5993466bc1577d6ad483181
Subrata34 <subratamodak3491@gmail.com>
2024-05-16 18:20:39 +0600

    files

---
+++ IfElse.py
@@ -0,0 +1,19 @@
+#conditions of python
+a=60
+b=70
+if a<b :
+    print("a less than b")
+else :
+    print("b less than a  ")
+
+#input value 
+f=input("First number of input :")
+c=input("2nd number of input :")
+if f>c :
+    print("f gether than c")
+elif  c>f :
+    print("c gather than f ")
+else :
+    print("c equal f ")
+
+
