f9ea262c05870b86f55e764086808a4a186b9da7
Subrata34 <subratamodak3491@gmail.com>
2024-05-16 17:56:23 +0600

    files

---
+++ studentInfo.py
@@ -0,0 +1,12 @@
+#copy method 
+studentInfo={
+    "sum1":20,
+    "sum2":24,
+    "sum3":26
+
+}
+print(studentInfo)
+mydicts=studentInfo.copy()
+print(mydicts)
+x=dict(studentInfo)  #copy right 
+print(x)
