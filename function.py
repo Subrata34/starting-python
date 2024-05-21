bb6d68bf81f301d56af886acacbfed4857196929
Subrata34 <subratamodak3491@gmail.com>
2024-05-21 17:17:26 +0600

    files

--- function.py
+++ function.py
@@ -3,5 +3,14 @@ def HabluFun(a,b):
     sum=a+b
     print(sum)
 
+def Minus(p,q):
+    minus=p-q
+    print(minus)
+
 HabluFun(20,60)
 HabluFun(20,40)
+HabluFun(90,70)
+Minus(400,70.89)
+Minus(400,90.65)
+
+
