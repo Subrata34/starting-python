2649e8ffbd2ba84515d778156572cdb0445bd4eb
Subrata34 <subratamodak3491@gmail.com>
2024-05-21 17:32:20 +0600

    files

---
+++ breakcontinue.py
@@ -0,0 +1,11 @@
+for x in range(9):
+    if x==6:
+        break
+    print(x)
+
+listarry=['tuply','sakib','mahin','tipu','tintin']
+
+for p in range(len(listarry)):
+    if p==2:
+     continue
+    print(p)
