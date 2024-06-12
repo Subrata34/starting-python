9e41a0c04b5e042a08501c720bbfe12bd317a305
Subrata34 <subratamodak3491@gmail.com>
2024-06-12 18:28:01 +0600

    files

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class robin:
+    car="BMW"
+    TK="200000"
+    HomePrice="110 corer  "
+
+
+class robina(robin):
+    brokenSmartphone="samsung glaxy "
+    brokencar="corola croch"
+
+
+r=robina()
+print(r.car)
